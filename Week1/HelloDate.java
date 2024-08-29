65e72cf46056a939674c79958a75d032d5456d20
Luu <vuluu0912@gmail.com>
2024-08-29 09:01:49 +0000

    Add Week1 to Weekly_exercises

---
+++ Week1/HelloDate.java
@@ -0,0 +1,11 @@
+package Week1;
+
+import java.util.*;
+
+
+public class HelloDate {
+ public static void main(String[] args) {
+ System.out.println("Hello, it is");
+ System.out.println(new Date());
+ }
+}
