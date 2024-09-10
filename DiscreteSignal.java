feb1549e3f0dae3e1478440fde118bcb92f50afd
Nguyễn Minh Đức <23010171@st.phenikaa-uni.edu.vn>
2024-09-10 11:53:45 +0700

    signal , Discrete signal and Radar

---
+++ DiscreteSignal.java
@@ -0,0 +1,34 @@
+//Ý 2
+public class DiscreteSignal implements Signal {
+    private double amplitude;
+    private double frequency;
+    private double period;
+    private double wavelength;
+
+    public DiscreteSignal(double amplitude, double frequency, double period, double wavelength) {
+        this.amplitude = amplitude;
+        this.frequency = frequency;
+        this.period = period;
+        this.wavelength = wavelength;
+    }
+
+    @Override
+    public double getAmplitude() {
+        return amplitude;
+    }
+
+    @Override
+    public double getFrequency() {
+        return frequency;
+    }
+
+    @Override
+    public double getPeriod() {
+        return period;
+    }
+
+    @Override
+    public double getWavelength() {
+        return wavelength;
+    }
+}
---
+++ Signal.java
@@ -0,0 +1,8 @@
+//Bài 1
+//Ý 1
+public interface Signal {
+    double getAmplitude();   
+    double getFrequency(); 
+    double getPeriod();     
+    double getWavelength(); 
+}
